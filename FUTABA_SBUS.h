c6ece0b18f05362944f3cbfa04decb3106bc7509
Sebastian Brandt <bastibrandt@gmail.com>
2015-01-31 17:44:34 +0100

    added project files for vs

--- FUTABA_SBUS.h
+++ FUTABA_SBUS.h
@@ -12,37 +12,37 @@
 
 class FUTABA_SBUS
 {
-  public:
-    void begin(void);
+public:
+	void begin(void);
 
-    void ProcessInput(void);
-    bool FetchChannelData(int16_t *pTarget, uint8_t &rStatusByte);
+	void ProcessInput(void);
+	bool FetchChannelData(int16_t *pTarget, uint8_t &rStatusByte);
 
-    bool IsDataAvailable() const { return m_bDataAvailable; }
-  
-  private:
-    void ItlStartReadingPayload();
-    void ItlAbortReadingPayload();
-    void ItlFinishReadingPayload();
-    void ItlSuccessfullReadPayload();
-    
-    /// Tries to resync by reading bytes until the searched byte is found and consumed
-    /// returns true if searched byte was found and consumed, false if no data is left and it was not found
-    bool ItlResyncTo(uint8_t nSearchedByte);
-    
-    bool m_bDataAvailable;
-    bool m_bIsReadingPayload;
-    
-    int  m_iCurBufferIndex;
-    
-    // A temporary buffer, used to 
-    // process the input data. If the line is 
-    // completely received and valid, it is copied to m_pReadSBusData
-    uint8_t m_pTempInBuffer[25];
-    
-    // The latest read sbus data
-    uint8_t m_pReadSBusData[25];
-                
+	bool IsDataAvailable() const { return m_bDataAvailable; }
+
+private:
+	void ItlStartReadingPayload();
+	void ItlAbortReadingPayload();
+	void ItlFinishReadingPayload();
+	void ItlSuccessfullReadPayload();
+
+	/// Tries to resync by reading bytes until the searched byte is found and consumed
+	/// returns true if searched byte was found and consumed, false if no data is left and it was not found
+	bool ItlResyncTo(uint8_t nSearchedByte);
+
+	bool m_bDataAvailable;
+	bool m_bIsReadingPayload;
+
+	int  m_iCurBufferIndex;
+
+	// A temporary buffer, used to 
+	// process the input data. If the line is 
+	// completely received and valid, it is copied to m_pReadSBusData
+	uint8_t m_pTempInBuffer[25];
+
+	// The latest read sbus data
+	uint8_t m_pReadSBusData[25];
+
 
 };
 
